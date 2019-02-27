4a03c7f8d33b904b65a35085574a657511c4de90
liammoroney <liam.moroney14@gmail.com>
2019-02-27 20:03:04 +0000

    MVP which allows you to enter a token, then play songs via this app and the song info will be shown. Also added buttons to pause/play and switch songs.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,162 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      token: "",
+      deviceId: "",
+      loggedIn: false,
+      error: "",
+      trackName: "Track Name",
+      artistName: "Artist Name",
+      albumName: "Album Name",
+      playing: false,
+      position: 0,
+      duration: 0,
+    };
+    this.playerCheckInterval = null;
+  }
+
+  onStateChanged(state) {
+    // if we're no longer listening to music, we'll get a null state.
+    if (state !== null) {
+      const {
+        current_track: currentTrack,
+        position,
+        duration,
+      } = state.track_window;
+      const trackName = currentTrack.name;
+      const albumName = currentTrack.album.name;
+      const artistName = currentTrack.artists
+        .map(artist => artist.name)
+        .join(", ");
+      const playing = !state.paused;
+      this.setState({
+        position,
+        duration,
+        trackName,
+        albumName,
+        artistName,
+        playing
+      });
+    }
+  }
+
+  handleLogin() {
+    if (this.state.token !== "") {
+      this.setState({ loggedIn: true });
+      // check every second for the player.
+      this.playerCheckInterval = setInterval(() => this.checkForPlayer(), 1000);
+    }
+  }
+
+  createEventHandlers() {
+    this.player.on('initialization_error', e => { console.error(e); });
+    this.player.on('authentication_error', e => {
+      console.error(e);
+      this.setState({ loggedIn: false });
+    });
+    this.player.on('account_error', e => { console.error(e); });
+    this.player.on('playback_error', e => { console.error(e); });
+
+    this.player.on('player_state_changed', state => this.onStateChanged(state));
+
+    this.player.on('ready', async ({ device_id }) => {
+      console.log("Let the music play on!");
+      await this.setState({ deviceId: device_id });
+      this.transferPlaybackHere();
+    });
+
+    this.player.addListener('not_ready', ({ device_id }) => {
+      console.log('Device ID has gone offline', device_id);
+    });
+  }
+
+  checkForPlayer() {
+    const { token } = this.state;
+    console.log(window)
+    if (window.Spotify !== null) {
+      clearInterval(this.playerCheckInterval);
+      this.player = new window.Spotify.Player({
+        name: "React Spotify Player",
+        getOAuthToken: cb => { cb(token); },
+      });
+      this.createEventHandlers();
+      this.player.connect();
+    }
+  }
+
+  onPrevClick() { this.player.previousTrack(); }
+
+  onPlayClick() { this.player.togglePlay(); }
+
+  onNextClick() { this.player.nextTrack(); }
+
+  transferPlaybackHere() {
+    const { deviceId, token } = this.state;
+    fetch("https://api.spotify.com/v1/me/player", {
+      method: "PUT",
+      headers: {
+        authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        "device_ids": [deviceId],
+        "play": true,
+      }),
+    });
+  }
+
   render() {
+    const {
+      token,
+      loggedIn,
+      artistName,
+      trackName,
+      albumName,
+      error,
+      position,
+      duration,
+      playing,
+    } = this.state;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="App-header">
+          <h2>React Spotify Player</h2>
+
+          {error && <p>Error: {error}</p>}
+
+          {loggedIn ?
+            (<div>
+              <p>Artist: {artistName}</p>
+              <p>Track: {trackName}</p>
+              <p>Album: {albumName}</p>
+              <p>
+                <button onClick={() => this.onPrevClick()}>Previous</button>
+                <button onClick={() => this.onPlayClick()}>{playing ? "Pause" : "Play"}</button>
+                <button onClick={() => this.onNextClick()}>Next</button>
+              </p>
+            </div>)
+            :
+            (<div>
+              <p className="App-intro">
+                Enter your Spotify access token. Get it from{" "}
+                <a href="https://beta.developer.spotify.com/documentation/web-playback-sdk/quick-start/#authenticating-with-spotify">
+                  here
+                </a>.
+              </p>
+              <p>
+                <input type="text" value={token} onChange={e => this.setState({ token: e.target.value })} />
+              </p>
+              <p>
+                <button onClick={() => this.handleLogin()}>Go</button>
+              </p>
+            </div>)
+          }
+        </div>
       </div>
     );
   }
