c7119e76e6c0d61040780d49cc0b6f05fc62b67b
liammoroney <liam.moroney14@gmail.com>
2019-02-28 09:56:37 +0000

    Added snackbar messages for spotify events. Display current track position/duration.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import Snackbar from '@material-ui/core/Snackbar';
+import MySnackbarContentWrapper from './Components/MySnackbarContentWrapper';
+import grey from '@material-ui/core/colors/grey';
+import Icon from '@material-ui/core/Icon';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit,
+  },
+  buttonHover: {
+    margin: theme.spacing.unit * 2,
+    color: grey[500],
+    '&:hover': {
+      color: 'white',
+    },
+  },
+  margin: {
+    margin: theme.spacing.unit,
+  },
+});
 
 class App extends Component {
   constructor(props) {
@@ -16,6 +39,7 @@ class App extends Component {
       playing: false,
       position: 0,
       duration: 0,
+      snackbarOpen: false
     };
     this.playerCheckInterval = null;
   }
@@ -24,25 +48,22 @@ class App extends Component {
     // State is null when music stops
     if (state !== null) {
       const {
-        current_track: currentTrack,
+        current_track: currentTrack
+      } = state.track_window;
+      const {
         position,
         duration,
-      } = state.track_window;
-      console.log(currentTrack)
-      const albumArt = currentTrack.album.images[0].url;
-      const trackName = currentTrack.name;
-      const albumName = currentTrack.album.name;
-      const artistName = currentTrack.artists
-        .map(artist => artist.name)
-        .join(", ");
+      } = state;
       const playing = !state.paused;
       this.setState({
         position,
         duration,
-        trackName,
-        albumName,
-        albumArt,
-        artistName,
+        trackName: currentTrack.name,
+        albumName: currentTrack.album.name,
+        albumArt: currentTrack.album.images[0].url,
+        artistName: currentTrack.artists
+          .map(artist => artist.name)
+          .join(", "),
         playing
       });
     }
@@ -56,24 +77,59 @@ class App extends Component {
   }
 
   createEventHandlers() {
-    this.player.on('initialization_error', e => { console.error(e); });
+    this.player.on('initialization_error', e => {
+      console.error(e);
+      this.setState({
+        snackbarOpen: true,
+        snackbarVariant: "error",
+        snackbarMessage: "Initialization error"
+      });
+    });
     this.player.on('authentication_error', e => {
       console.error(e);
-      this.setState({ loggedIn: false });
+      this.setState({
+        loggedIn: false,
+        snackbarOpen: true,
+        snackbarVariant: "error",
+        snackbarMessage: "Authentication error"
+      });
     });
-    this.player.on('account_error', e => { console.error(e); });
-    this.player.on('playback_error', e => { console.error(e); });
+    this.player.on('account_error', e => {
+      console.error(e);
+      this.setState({
+        snackbarOpen: true,
+        snackbarVariant: "error",
+        snackbarMessage: "Account error"
+      });
+    });
+    this.player.on('playback_error', e => {
+      console.error(e);
+      this.setState({
+        snackbarOpen: true,
+        snackbarVariant: "warning",
+        snackbarMessage: "Playback error"
+      });
+    });
 
     this.player.on('player_state_changed', state => this.onStateChanged(state));
 
     this.player.on('ready', async ({ device_id }) => {
-      console.log("Let the music play on!");
-      await this.setState({ deviceId: device_id });
+      await this.setState({
+        deviceId: device_id,
+        snackbarOpen: true,
+        snackbarVariant: "success",
+        snackbarMessage: "Connected!"
+      });
       this.transferPlaybackHere();
     });
 
     this.player.addListener('not_ready', ({ device_id }) => {
       console.log('Device ID has gone offline', device_id);
+      this.setState({
+        snackbarOpen: true,
+        snackbarVariant: "info",
+        snackbarMessage: "Device has gone offline"
+      });
     });
   }
 
@@ -111,7 +167,12 @@ class App extends Component {
     });
   }
 
+  handleClose = (event, reason) => {
+    if (reason !== 'clickaway') { this.setState({ snackbarOpen: false }); }
+  };
+
   render() {
+    const { classes } = this.props;
     const {
       token,
       loggedIn,
@@ -123,6 +184,9 @@ class App extends Component {
       position,
       duration,
       playing,
+      snackbarOpen,
+      snackbarVariant,
+      snackbarMessage
     } = this.state;
 
     return (
@@ -137,11 +201,34 @@ class App extends Component {
               <p>Artist: {artistName}</p>
               <p>Track: {trackName}</p>
               <p>Album: {albumName}</p>
+              <p>Position: {position}</p>
+              <p>Duration: {duration}</p>
               {albumArt && <img src={albumArt} alt="albumArt" />}
               <p>
-                <button onClick={() => this.onPrevClick()}>Previous</button>
-                <button onClick={() => this.onPlayClick()}>{playing ? "Pause" : "Play"}</button>
-                <button onClick={() => this.onNextClick()}>Next</button>
+                <Icon
+                  className={classes.buttonHover}
+                  color="primary"
+                  fontSize="large"
+                  onClick={() => this.onPrevClick()}
+                >
+                  skip_previous
+                </Icon>
+                <Icon
+                  className={classes.buttonHover}
+                  color="primary"
+                  fontSize="large"
+                  onClick={() => this.onPlayClick()}
+                >
+                  {playing ? 'pause' : 'play_arrow'}
+                </Icon>
+                <Icon
+                  className={classes.buttonHover}
+                  color="primary"
+                  fontSize="large"
+                  onClick={() => this.onNextClick()}
+                >
+                  skip_next
+                </Icon>
               </p>
             </div>)
             :
@@ -161,9 +248,24 @@ class App extends Component {
             </div>)
           }
         </div>
+        <Snackbar
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}
+          open={snackbarOpen}
+          autoHideDuration={6000}
+          onClose={this.handleClose}
+        >
+          <MySnackbarContentWrapper
+            onClose={this.handleClose}
+            variant={snackbarVariant}
+            message={snackbarMessage}
+          />
+        </Snackbar>
       </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
